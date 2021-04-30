5acaee9f7251774993fe16ef3a83c46e88d2cf88
salini03 <salini.vallathol@gmail.com>
2021-04-30 09:19:08 +0530

    code refactor

---
+++ src/main/java/com/institute/businessplanner/BusinessplannerApplication.java
@@ -0,0 +1,13 @@
+package com.institute.businessplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/institute/businessplanner/BusinessplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institute.businessplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
